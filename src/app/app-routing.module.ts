782b85c896f28be85002193cc4c80e7b1058b25c
raquelvilas <quelpsi@gmail.com>
2023-11-15 21:34:22 -0300

    adding home and project-card components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'projects',
+    component: HomeComponent
+  }
+  ,
+  {
+    path: '',
+    redirectTo: 'projects',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,25 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { ButtonPrimaryComponent } from './components/button-primary/button-primary.component';
-import { ButtonSecondaryComponent } from './components/button-secondary/button-secondary.component';
-
 import '@material/web/button/outlined-button.js';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonPrimaryComponent,
-    ButtonSecondaryComponent,
+    ProjectCardComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    MatButtonModule,  
   ],
 
   schemas: [
